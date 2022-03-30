902be98942fb5d15a03881927f623f05c0890fa5
Marc <marcllopis89@gmail.com>
2022-03-30 12:34:52 +0200

    Adds more components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Header from "./components/Header";
 import Paragraph from "./components/Paragraph";
+import StudentInfo from "./components/StudentInfo";
 import Welcome from "./components/Welcome";
 import "./App.css";
+
+let studentResume =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate obcaecati doloribus eius ea optio facilis laborum, sapiente illo odio, veniam cumque sed eligendi quisquam delectus? Amet mollitia, nam placeat officiis est laudantium rerum exercitationem ea! Voluptatem perferendis reiciendis natus quod reprehenderit delectus aperiam inventore repellendus exercitationem earum maxime nam provident odit illo quisquam assumenda laudantium a, sit temporibus consectetur quo magni officia tenetur voluptatibus. Vel ducimus voluptas sed, eveniet molestias rerum harum labore repudiandae perferendis molestiae nihil ipsam iste qui ab cum esse laborum repellendus tempore blanditiis provident. Tenetur, a!";
 
 let social = ["Facebook", "Instagram", "Twitter"];
 
@@ -16,6 +20,8 @@ function App() {
       <Header title="marc title" subtitle="marc subtitle" />
       <Header title="extra stuff" subtitle="more extra subtitle" />
       <Welcome />
+      <StudentInfo name="Marc" location="Barcelona" info={studentResume} />
+      <StudentInfo name="Billy" location="France" info={studentResume} />
       <Paragraph />
       <Paragraph />
       <button>Enter</button>
---
+++ src/components/StudentDetails.js
@@ -0,0 +1,9 @@
+const StudentDetails = (props) => (
+  <>
+    <h3>Student info</h3>
+    <p>Name: {props.firstName}</p>
+    <p>Location: {props.location}</p>
+  </>
+);
+
+export default StudentDetails;
