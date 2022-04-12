16c5a26eb69fc90c1a537cb0b9aa9e5ff9c61da6
Billy <billy.m.nightingale@gmail.com>
2022-04-12 13:56:04 +0200

    bonus: only display winner when selected

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
+import "./App.css"
 import React, {useState, useEffect} from "react"; 
-
-
 
 function App() {
   let [joke, setJoke] = useState({});
   let [joke2, setJoke2] = useState({});
-  let [winner, setWinner] = useState(false);
+  let [winner, setWinner] = useState("");
 
+  function refresh() {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setJoke(data);
+      });
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => {
+          setJoke2(data);
+      });
+    setWinner("");
+  }
 
   useEffect(() => {
-  
-  fetch("https://api.chucknorris.io/jokes/random")
-  .then((response) => response.json())
-  .then((data) => {
-    setJoke(data);
-  });
-  fetch("https://api.chucknorris.io/jokes/random")
-  .then((response) => response.json())
-  .then((data) => {
-        setJoke2(data);
-      });
-}, []);
+    refresh();
+  }, []);
 
-const handleclick1 = (event) => {
-  setWinner(event.target.value);
-console.log("clicked!")
-};
+  const handleClick1 = () => {
+    setWinner(joke.value);
+  };
 
-const handleclick2 = (event) => {
-  setWinner(event.target.value);
-console.log("clicked!")
-};
+  const handleClick2 = () => {
+    setWinner(joke2.value);
+  };
+
 
   return (
     <div className="App">
       <h1>React project</h1>
-
-
+      {winner === "" ? (
+      <>
       <p>This is Chucks first joke: {joke.value}</p>
-      <button onClick={handleclick1}> Select this joke!</button>
+      <button onClick={handleClick1}> Select this joke!</button>
       <p>This is his second joke: {joke2.value}</p>
-      <button onClick={handleclick2}> Select this joke!</button>
+      <button onClick={handleClick2}> Select this joke!</button>
+      </>
+      ) : (
+      <>
+      <h1>The funniest joke was: {winner}</h1>
+      <button onClick={() => refresh()}>Refresh</button>
+      </>
+      )}
     </div>
   );
 }
