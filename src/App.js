6cb9eed722f6d0e449d2068cccb2f4707624f235
Billy <billy.m.nightingale@gmail.com>
2022-04-12 15:22:23 +0200

    refactored some of the code with Marc

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import "./App.css"
 import React, {useState, useEffect} from "react"; 
 
 function App() {
-  let [joke, setJoke] = useState({});
-  let [joke2, setJoke2] = useState({});
+  let [joke, setJoke] = useState("");
+  let [joke2, setJoke2] = useState("");
   let [winner, setWinner] = useState("");
 
   function refresh() {
     fetch("https://api.chucknorris.io/jokes/random")
       .then((response) => response.json())
       .then((data) => {
-        setJoke(data);
+        setJoke(data.value);
       });
     fetch("https://api.chucknorris.io/jokes/random")
       .then((response) => response.json())
       .then((data) => {
-          setJoke2(data);
+          setJoke2(data.value);
       });
     setWinner("");
   }
@@ -24,29 +24,24 @@ function App() {
     refresh();
   }, []);
 
-  const handleClick1 = () => {
-    setWinner(joke.value);
+  const handleClick = (selectedJoke) => {
+    setWinner(selectedJoke);
   };
-
-  const handleClick2 = () => {
-    setWinner(joke2.value);
-  };
-
 
   return (
     <div className="App">
       <h1>React project</h1>
       {winner === "" ? (
       <>
-      <p>This is Chucks first joke: {joke.value}</p>
-      <button onClick={handleClick1}> Select this joke!</button>
-      <p>This is his second joke: {joke2.value}</p>
-      <button onClick={handleClick2}> Select this joke!</button>
+      <p>This is Chucks first joke: {joke}</p>
+      <button onClick={() => handleClick(joke)}> Select this joke!</button>
+      <p>This is his second joke: {joke2}</p>
+      <button onClick={() => handleClick(joke2)}> Select this joke!</button>
       </>
       ) : (
       <>
       <h1>The funniest joke was: {winner}</h1>
-      <button onClick={() => refresh()}>Refresh</button>
+      <button onClick={refresh}>Refresh</button>
       </>
       )}
     </div>
