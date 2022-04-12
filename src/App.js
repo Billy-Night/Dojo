bf04621d744d870068fecc33505a13a362046545
Billy <billy.m.nightingale@gmail.com>
2022-04-12 11:02:53 +0200

    question 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import React, {useState, useEffect} from "react"; 
+
+
+
 function App() {
+  let [joke, setJoke] = useState({});
+  let [joke2, setJoke2] = useState({});
+  let [winner, setWinner] = useState(false);
+
+
+  useEffect(() => {
+  
+  fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => {
+    setJoke(data);
+  });
+  fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => {
+        setJoke2(data);
+      });
+}, []);
+
+const handleclick1 = (event) => {
+  setWinner(event.target.value);
+console.log("clicked!")
+};
+
+const handleclick2 = (event) => {
+  setWinner(event.target.value);
+console.log("clicked!")
+};
+
   return (
     <div className="App">
       <h1>React project</h1>
+
+
+      <p>This is Chucks first joke: {joke.value}</p>
+      <button onClick={handleclick1}> Select this joke!</button>
+      <p>This is his second joke: {joke2.value}</p>
+      <button onClick={handleclick2}> Select this joke!</button>
     </div>
   );
 }
